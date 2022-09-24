9076d568bfca131c1a11922fadfc2bb58d267fd2
pr0317 <kevinbernalk@gmail.com>
2022-09-23 20:47:29 -0500

    Modified chart.

--- pages/charts/index.tsx
+++ pages/charts/index.tsx
@@ -4,52 +4,68 @@ import Chart from 'chart.js';
 
 class Charts extends Component {
 
-    donutData: object = { //cambiar a barlinecombined 
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
+  blue: String = "";
+
+
+  barCombinedLine: object = {
+        labels: ["Amparo","Seddik","M",
+        "Laarbi","Grietje","Nacor","Soufia",
+        "Laura","Rogers","Annalisa","Els","Jimmie",
+        "Braulia","Salete","Marinel","Maimoun","Quique",
+        "Alexsandro","Alta","Jessenia","Etor","Pelegri","Isabel","Zabulon","Youcef","Dunia","Elisabete","Brahima","Danya","Bart"],
+        datasets: [
+        {
+          label: 'taller',
+          data: [3,1,3,3,3,4,3,4,3,3,2,2,5,2,4,2,1,5,4,1,2,3,5,3,4,5,2,5,3,4],
+          borderColor: "#CACFD2",
+          backgroundColor: "#884EA0",
+          order: 1
+        },
+        {
+          label: 'investigacion',
+          data: [5,5,2,5,1,2,1,5,1,2,4,4,4,3,4,3,5,2,1,2,2,5,4,4,5,3,1,5,4,2],
+          borderColor: "#CACFD2",
+          backgroundColor: "#F39C12",
+          order: 1
+        },
+        {
+          label: 'parcial',
+          data: [1,4,4,3,5,1,5,4,2,1,2,5,4,3,5,3,4,1,1,2,1,5,1,2,5,5,3,4,5,3],
+          borderColor: "#CACFD2",
+          backgroundColor: "#5499C7",
+          order: 1
+        },
+        {
+          label: 'definitiva',
+          data: [3,3.33,3,3.67,3,2.33,3,4.33,2,2,2.67,3.67,4.33,2.67,4.33,2.67,3.33,2.67,2,1.67,1.67,4.33,3.33,3,4.67,4.33,2,4.67,4,3],
+          borderColor: "#F4D03F",
+          backgroundColor: "#F4D03F",
+          type: 'line',
+          order: 0
+        }
+      ]
+    };
+
+  componentDidMount(){ //render component charts
+      const canvas: any = document.getElementById('graph_2') as HTMLCanvasElement;
+      const ctx: any = canvas.getContext('2d');
+      const config = {
+        type: 'bar',
+        data: this.barCombinedLine,          
       };
+      new Chart(ctx,config);        
+      //const graph_2 = new Chart(ctx,config);
+  }
+  
 
-    componentDidMount(){ //render component charts
-        const canvas: any = document.getElementById('myChart') as HTMLCanvasElement;
-        const ctx: any = canvas.getContext('2d');
-        const config = {
-            type: 'doughnut',
-            data: this.donutData,            
-        };
-        /*const myChart = new Chart(ctx,config);
-
-        const canvas_2: any = document.getElementById('myChart_2') as HTMLCanvasElement;
-        const ctx_2: any = canvas_2.getContext('2d');
-        const config_2 = {
-            type: 'doughnut',
-            data: this.donutData,            
-        };
-        const myChart_2 = new Chart(ctx_2,config_2);*/
-    }
-    
-//<canvas id="myChart_2" width="400" height="400"></canvas>
-
-    render() { 
-        return <>
-        <div className = 'w-[300px]'>
-          <h1>Donut Graph Chart</h1>
-          <canvas id="myChart" width="400" height="400"></canvas>          
-        </div>
-        </>
-    }
+  render() { 
+      return <>
+      <div className = 'w-[600px]'>
+        <h1>Combined Bar+Line Graph Chart</h1>
+        <canvas id="graph_2" width="1800" height="900"></canvas>          
+      </div>
+      </>
+  }
 }
 
 export default Charts
